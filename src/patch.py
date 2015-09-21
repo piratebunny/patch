60f432a07b99064b2697001c9b48cc4b99936af3
Gary <gary.martin@physics.org>
2015-09-21 16:03:41 +0100

    Playing with mouse coords and pep8 tidying.

--- src/patch.py
+++ src/patch.py
@@ -1,32 +1,31 @@
 import pi3d
 import math
-import random
 
 KEYBOARD = pi3d.Keyboard()
 LOGGER = pi3d.Log.logger(__name__)
 
-BACKGROUND_COLOR = (1.0, 1.0, 1.0, 0.0)
+BACKGROUND_COLOR = (0.4, 0.8, 0.8, 1)
 
-#DISPLAY = pi3d.Display.create(tk=True, background=BACKGROUND_COLOR)
-#winw, winh, bord = 1200, 600, 0     #64MB GPU memory setting
+LEFT_BUTTON = 9
+RIGHT_BUTTON = 10
+
+winw, winh, bord = 1200, 600, 0     # 64MB GPU memory setting
 DISPLAY = pi3d.Display.create(tk=True, window_title='Tiger Tank demo in Pi3D',
-                              #                              w=winw, h=winh - bord, far=3000.0,
-                              background=(0.4, 0.8, 0.8, 1), frames_per_second=16)
+                              w=winw, h=winh - bord, far=3000.0,
+                              background=BACKGROUND_COLOR, frames_per_second=16)
 WIDTH, HEIGHT = DISPLAY.width, DISPLAY.height
 ZPLANE = 1000
 fov = 2.0 * math.degrees(math.atan(HEIGHT/2.0/ZPLANE))
 win = DISPLAY.tkwin
 
-CAMERA = pi3d.Camera((0, 0, 0), (0, 0, -1.0),
-                     (1, 1100, fov,
-                      WIDTH / float(HEIGHT)))
+CAMERA = pi3d.Camera(is_3d=False)
 SHADER = pi3d.Shader('uv_flat')
 
-TEXTURE_NAMES = ['../graphics/piratebunny.png']
+TEXTURE_NAMES = ['../graphics/piratebunny2.png']
 TEXTURES = [pi3d.Texture(t) for t in TEXTURE_NAMES]
 
-#key presses
-mymouse = pi3d.Mouse(restrict = False)
+# key presses
+mymouse = pi3d.Mouse(restrict=False)
 mymouse.start()
 omx, omy = mymouse.position()
 
@@ -36,20 +35,26 @@ class Block(pi3d.ImageSprite):
     def __init__(self, camera=None, shader=None, texture=None,
                  x=10, y=0,
                  w=10.0, h=10.0, z=1000):
-        super(Block, self).__init__(camera=camera, texture=texture, shader=shader, w=w, h=h, x=x, y=y, z=z)
+        super(Block, self).__init__(camera=camera, texture=texture,
+                                    shader=shader, w=w, h=h, x=x, y=y, z=z)
+
+    def is_under_mouse(self, mx, my):
+        LOGGER.info("block centre %s %s", self.x(), self.y())
+        return ((self.x() - self.width) < mx < (self.x() + self.width)) and \
+            ((self.y() - self.height) < my < (self.y() + self.height))
 
 block = Block(texture=TEXTURES[0], shader=SHADER, w=50, h=50, camera=CAMERA)
 SPRITES = [block]
 DISPLAY.add_sprites(*SPRITES)
 
+
+def find_blocks_under_mouse(mx, my):
+    return (s for s in SPRITES if s.is_under_mouse(mx, my))
+
 while DISPLAY.loop_running():
 
-    #mx, my = mymouse.position()
-    button_status =mymouse.button_status()
-    #print(mx)
-    #print(my)
-    #print(button_status)
-    #print('\n')
+    mx, my = mymouse.position()
+    button_status = mymouse.button_status()
 
     try:
         win.update()
@@ -85,8 +90,13 @@ while DISPLAY.loop_running():
             block.translateX(-10)
         elif win.key == 'd':
             block.translateX(10)
-    if win.ev == "click" or win.ev == "drag":
-        print("Click")
-        block.translateX(100)
-    else:
-        win.ev=""  #clear the event so it doesn't repeat
+
+    if button_status == LEFT_BUTTON:
+        LOGGER.info("LEFT_BUTTON")
+        LOGGER.info("thing %s %s", mx, my)
+        for block in find_blocks_under_mouse(mx, my):
+            block.translateX(10)
+    if button_status == RIGHT_BUTTON:
+        LOGGER.info("RIGHT_BUTTON")
+        for block in find_blocks_under_mouse(mx, my):
+            block.translateX(-10)
